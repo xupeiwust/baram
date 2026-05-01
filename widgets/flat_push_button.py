ddcaf0d1c88e6ccda65e5b44f876c84c3affe76b
jake <jakejiwon@gmail.com>
2026-05-01 16:22:47 +0900

    Issue baram#805 To use theme's highlight color instead of hard-coded one

--- widgets/flat_push_button.py
+++ widgets/flat_push_button.py
@@ -3,13 +3,11 @@
 
 from PySide6.QtWidgets import QPushButton, QStyle, QStyleOptionButton, QStylePainter
 from PySide6.QtCore import Qt, Signal
-from PySide6.QtGui import QColor, QMouseEvent, QPainter
+from PySide6.QtGui import QMouseEvent, QPainter, QPalette
 
 
 class FlatPushButton(QPushButton):
     doubleClicked = Signal()
-
-    _CHECKED_BG = QColor(76, 194, 255, 100)  # Dart mode highlight color of Windows
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -19,17 +17,19 @@ class FlatPushButton(QPushButton):
         self.initStyleOption(option)
         if option.state & QStyle.StateFlag.State_On:
             rect = self.style().subElementRect(QStyle.SubElement.SE_PushButtonContents, option, self)
+            color = self.palette().color(QPalette.ColorRole.Highlight)
+            color.setAlpha(100)  # just a good-looking value
             painter = QPainter(self)
             painter.setRenderHint(QPainter.RenderHint.Antialiasing)
             painter.setPen(Qt.PenStyle.NoPen)
-            painter.setBrush(self._CHECKED_BG)
+            painter.setBrush(color)
             painter.drawRoundedRect(rect, 4, 4)
             painter.end()
             sp = QStylePainter(self)
             sp.drawControl(QStyle.ControlElement.CE_PushButtonLabel, option)
         elif option.state & (QStyle.StateFlag.State_MouseOver | QStyle.StateFlag.State_Sunken):
             super().paintEvent(event)
-        else:
+        else:  # transparent background
             painter = QStylePainter(self)
             painter.drawControl(QStyle.ControlElement.CE_PushButtonLabel, option)
 
