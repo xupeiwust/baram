4c992d696dce3fd776df37786eeafaab5513bde6
jake <jakejiwon@gmail.com>
2026-05-26 11:50:50 +0900

    Issue baram#846 Defensive code to handle the step value beyond EXPORT

--- baramMesh/view/main_window/step_manager.py
+++ baramMesh/view/main_window/step_manager.py
@@ -139,6 +139,8 @@ class StepManager(QObject):
         return await self.currentPage().save()
 
     def openNextStep(self):
+        if self._workingStep >= Step.LAST_STEP:
+            return
         self._open(self._workingStep + 1)
 
     def retranslatePages(self):
@@ -157,7 +159,7 @@ class StepManager(QObject):
             self._pages[step].stepReset.connect(self._buttons.disableNextButton)
 
         for step in range(Step.CASTELLATION, Step.EXPORT):
-            self._pages[step].stepCompleted.connect(lambda: self._buttons.showButton(ButtonID.NEXT))
+            self._pages[step].stepCompleted.connect(self._onIntermediateStepCompleted)
             self._pages[step].stepReset.connect(lambda: self._buttons.showButton(ButtonID.FINISH))
 
         self._pages[Step.GEOMETRY].geometryRemoved.connect(self._geometryRemoved)
@@ -204,6 +206,9 @@ class StepManager(QObject):
 
     @qasync.asyncSlot()
     async def _finishSteps(self):
+        if self._batchRunning:
+            return
+
         self._batchRunning = True
         self._buttons.showButton(ButtonID.CANCEL)
 
@@ -221,6 +226,9 @@ class StepManager(QObject):
         else:
             await AsyncMessageBox().information(self._contentStack, self.tr('Process Completed'),
                                                 self.tr('All steps complete.'))
+
+        if self._workingStep > Step.LAST_STEP:
+            self._workingStep = Step.LAST_STEP
 
         # Apply current workingStep
         self._setWorkingStep(self._workingStep)
@@ -266,6 +274,10 @@ class StepManager(QObject):
     def _geometryRemoved(self):
         self._pages[Step.CASTELLATION].unload()
         self._pages[Step.BOUNDARY_LAYER].unload()
+
+    def _onIntermediateStepCompleted(self):
+        if self._navigation.currentStep() < Step.LAST_STEP:
+            self._buttons.showButton(ButtonID.NEXT)
 
     def _updateControlButtons(self, step):
         if self._batchRunning:
